3b688a38cdf6b1ab68ed00a85756949d68bca3f3
Android Studio <devandroid@ort>
2022-08-08 10:07:41 -0300

    arbol

--- src/App.js
+++ src/App.js
@@ -7,59 +7,35 @@ import foto from './a.jpg';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useState } from "react";
 
 function App() {
+
+  const [citas, setCitas] = useState([{
+    mascota: "Nina",
+    dueño: "Martin",
+    fecha: "2021-08-05",
+    hora: "08:20",
+    sintomas: "Le duele la pierna"
+  }])
+
   return (
-    <div className="App row" >
+    <div className="App  " >
 
-      <h1>ADMINISTRADOR DE PACIENTES </h1>
-      <h1>CREAR MI CITA </h1>
-      <h1>ADMINISTRA TUS CITAS </h1>
+      <h1 className='Titulo'>ADMINISTRADOR DE PACIENTES </h1>
 
-      <Form className="Form">
-
-        <Form.Group className="col-3" controlId="formBasicEmail">
-          <p className="TituloLabel">Nombre Mascota</p>
-          <Form.Control className="mb-3" type="email" placeholder="Enter email" />
+      <Row className='Posicion'>
 
 
-          <p className="TituloLabel">Nombre Dueño</p>
-          <Form.Control className="mb-3" type="email" placeholder="Enter email" />
+        <h1 className='Titulo col-4 '>CREAR MI CITA </h1>
+        <h1 className='Titulo col-4 '>ADMINISTRA TUS CITAS </h1>
+      </Row>
 
+      <Row>
+        <Formulario></Formulario>
+        <Cita></Cita>
+      </Row>
 
-          <p className="TituloLabel">Fecha</p>
-          <Form.Control  type="date" name="fecha" className="Input mb-3" />
-
-
-
-          <p className="TituloLabel">Hora</p>
-          <Form.Control type="time" name="hora" className="Input mb-3" />
-
-          <p className="TituloLabel">Sintomas</p>
-          <Form.Control as="textarea" style={{ height: '100px' }} className="mb-3" type="text"  />
-
-          
-        <Button className="Boton" variant="primary" type="submit">
-          AGREGAR CITA
-        </Button> 
-
-        </Form.Group>
-
-
-      </Form>
-
-
-      <Card className="col-3 " style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
 
       <div>
 
@@ -70,6 +46,81 @@ function App() {
 
 
   );
+}
+
+function GuardarCitasfunction() {
+
+}
+
+function Formulario({ setCitas }) {
+  const [mascota, setMascota] = useState("")
+  const [dueño, setDueño] = useState("")
+  const [fecha, setFecha] = useState("")
+  const [hora, setHora] = useState("")
+  const [sintomas, setSintomas] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCitas(prev => [...prev, {
+      mascota,
+      dueño,
+      fecha,
+      hora,
+      sintomas
+    }])
+    setMascota("")
+    setDueño("")
+    setFecha("")
+    setHora("")
+    setSintomas("")
+  }
+  return (
+    <Form onSubmit={handleSubmit} className="Form col-3">
+
+      <Form.Group className="" controlId="formBasicEmail">
+        <p className="TituloLabel">Nombre Mascota</p>
+        <Form.Control className="mb-3" type="text" placeholder="Enter email" />
+
+        <p className="TituloLabel">Nombre Dueño</p>
+        <Form.Control className="mb-3" type="text" placeholder="Enter email" />
+
+        <p className="TituloLabel">Fecha</p>
+        <Form.Control type="date" name="fecha" className="Input mb-3" />
+
+        <p className="TituloLabel">Hora</p>
+        <Form.Control type="time" name="hora" className="Input mb-3" />
+
+        <p className="TituloLabel">Sintomas</p>
+        <Form.Control as="textarea" style={{ height: '100px' }} className="mb-3" type="text" />
+
+
+        <Button className="Boton" variant="primary" type="submit">
+          AGREGAR CITA
+        </Button>
+
+      </Form.Group>
+
+
+    </Form>
+
+  )
+}
+
+function Cita(props) {
+  return (
+    <Card className="Tarjeta" style={{ width: '18rem' }}>
+      <Card.Body>
+
+        <Card.Text className='TextoCard'>Mascota: {props.mascota}</Card.Text>
+        <Card.Text>Dueño: {props.dueño}</Card.Text>
+        <Card.Text>Fecha:{props.fecha}</Card.Text>
+        <Card.Text>Hora:{props.hora}</Card.Text>
+        <Card.Text>Sintomas:{props.sintomas}</Card.Text>
+        <button onClick={props.delete}> ELIMINAR CITA </button>
+      </Card.Body>
+    </Card>
+  )
+
 }
 
 export default App;
