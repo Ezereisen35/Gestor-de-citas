b998336b1e98b1392a21776a051e4bb24d30a7a5
Android Studio <devandroid@ort>
2022-08-16 15:32:21 -0300

    termina3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import foto from './a.jpg';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
@@ -19,6 +19,10 @@ function App() {
     sintomas: "Le duele la pierna"
   }])
 
+  useEffect(() => {
+    console.log(citas)
+  })
+
   return (
     <div className="App" >
 
@@ -27,21 +31,18 @@ function App() {
       <Row className='Posicion'>
 
 
-        <h1 className='Titulo col-4 '>CREAR MI CITA </h1>
+        <h1 className='Titulo col-6 '>CREAR MI CITA </h1>
         <h1 className='Titulo col-4 '>ADMINISTRA TUS CITAS </h1>
       </Row>
 
       <Row>
-        <Formulario></Formulario>
-        <Cita citas={citas} setCitas={setCitas} />
+        
+        <Col><Formulario className="col-6"setCitas={setCitas}></Formulario></Col>
+        <Col><ListaCita className="col-6 ml-4" citas={citas} setCitas={setCitas} /></Col>
+        
       </Row>
 
 
-      <div>
-
-
-        <img src={foto} />
-      </div>
     </div>
 
 
@@ -75,23 +76,23 @@ function Formulario({ setCitas }) {
     setSintomas("")
   }
   return (
-    <Form onSubmit={handleSubmit} className="Form col-3">
+    <Form onSubmit={handleSubmit} className="Form col-6">
 
       <Form.Group className="" controlId="formBasicEmail">
         <p className="TituloLabel">Nombre Mascota</p>
-        <Form.Control className="mb-3" type="text" placeholder="Enter email" />
+        <Form.Control className="mb-3" type="text" placeholder="Enter email" value={mascota} onChange={e => setMascota(e.target.value)} />
 
         <p className="TituloLabel">Nombre Dueño</p>
-        <Form.Control className="mb-3" type="text" placeholder="Enter email" />
+        <Form.Control className="mb-3" type="text" placeholder="Enter email" value={dueño} onChange={e => setDueño(e.target.value)} />
 
         <p className="TituloLabel">Fecha</p>
-        <Form.Control type="date" name="fecha" className="Input mb-3" />
+        <Form.Control type="date" name="fecha" className="Input mb-3" value={fecha} onChange={e => setFecha(e.target.value)}/>
 
         <p className="TituloLabel">Hora</p>
-        <Form.Control type="time" name="hora" className="Input mb-3" />
+        <Form.Control type="time" name="hora" className="Input mb-3" value={hora} onChange={e => setHora(e.target.value)} />
 
         <p className="TituloLabel">Sintomas</p>
-        <Form.Control as="textarea" style={{ height: '100px' }} className="mb-3" type="text" />
+        <Form.Control as="textarea" style={{ height: '100px' }} className="mb-3" type="text" value={sintomas} onChange={e => setSintomas(e.target.value)}/>
 
 
         <Button className="Boton" variant="primary" type="submit">
@@ -106,19 +107,9 @@ function Formulario({ setCitas }) {
   )
 }
 
-function Cita(props) {
+function ListaCita(props) {
   return (
     <>
-      <Card className="Tarjeta" style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Text className='TextoCard'>Mascota: {props.mascota}</Card.Text>
-          <Card.Text>Dueño: {props.dueño}</Card.Text>
-          <Card.Text>Fecha:{props.fecha}</Card.Text>
-          <Card.Text>Hora:{props.hora}</Card.Text>
-          <Card.Text>Sintomas:{props.sintomas}</Card.Text>
-          <button onClick={props.delete}> ELIMINAR CITA </button>
-        </Card.Body>
-      </Card>
       <div className="flex flex-col gap-4">{props.citas.map((cita, i) => <Cita
                 {...cita}
                 delete={() => {
@@ -134,6 +125,23 @@ function Cita(props) {
     
   )
 
+}
+
+
+function Cita (props) {
+  console.log(props)
+  return (
+    <Card className="Tarjeta" style={{ width: '18rem' }}>
+    <Card.Body>
+      <Card.Text className='TextoCard'>Mascota: {props.mascota}</Card.Text>
+      <Card.Text>Dueño: {props.dueño}</Card.Text>
+      <Card.Text>Fecha:{props.fecha}</Card.Text>
+      <Card.Text>Hora:{props.hora}</Card.Text>
+      <Card.Text>Sintomas:{props.sintomas}</Card.Text>
+      <button onClick={props.delete}> ELIMINAR CITA </button>
+    </Card.Body>
+  </Card>
+  )
 }
 
 export default App;
